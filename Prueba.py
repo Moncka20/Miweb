bf3c226c79dee750a362c61678c4527928dc9e89
Thomas_Moncayo <moncayothomas@gmail.com>
2026-09-05 16:44:51 -0500

    añadir ci/cd

---
+++ Prueba.py
@@ -0,0 +1,8 @@
+def sumar(numero_a, numero_b):
+	"""Devuelve la suma de dos numeros."""
+	return numero_a + numero_b
+
+
+def multiplicar(numero_a, numero_b):
+	"""Devuelve el producto de dos numeros."""
+	return numero_a * numero_b
---
+++ test_prueba.py
@@ -0,0 +1,15 @@
+import unittest
+
+from Prueba import multiplicar, sumar
+
+
+class PruebaOperaciones(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(2, 3), 5)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(4, 3), 12)
+
+
+if __name__ == "__main__":
+    unittest.main()
