9a6f6ae88f7074364d3755b5ceeb9830b28b756a
Thomas_Moncayo <moncayothomas@gmail.com>
2026-09-19 15:21:45 -0500

    feat: agregar método resta() a Calculator con test unitario

--- Prueba.py
+++ Prueba.py
@@ -2,6 +2,9 @@ def sumar(numero_a, numero_b):
 	"Devuelve la suma de dos numeros."
 	return numero_a + numero_b
 
+def resta(numero_a, numero_b):
+    "Devuelve la resta de dos numeros."
+    return numero_a - numero_b
 
 def multiplicar(numero_a, numero_b):
 	"Devuelve el producto de dos numeros."
--- test_prueba.py
+++ test_prueba.py
@@ -1,11 +1,14 @@
 import unittest
 
-from Prueba import multiplicar, sumar
+from Prueba import multiplicar, sumar, resta
 
 
 class PruebaOperaciones(unittest.TestCase):
     def test_sumar(self):
         self.assertEqual(sumar(2, 3), 5)
+
+    def test_resta(self):
+        self.assertEqual(resta(5, 3), 2)
 
     def test_multiplicar(self):
         self.assertEqual(multiplicar(4, 3), 12)
